03b70ec9678ecfed26178091a88cc6d545751594
rahulkusuma1999 <55886415+rahulkusuma1999@users.noreply.github.com>
2020-05-03 13:00:48 +0530

    Add files via upload

---
+++ Time conversion.py
@@ -0,0 +1,17 @@
+
+'''
+
+ problem Statement :   https://www.hackerrank.com/challenges/time-conversion/problem
+
+
+'''
+
+
+
+
+
+time = input().strip()
+h, m, s = map(int, time[:-2].split(':'))
+p = time[-2:]
+h = h % 12 + (p.upper() == 'PM') * 12
+print(('%02d:%02d:%02d') % (h, m, s))
