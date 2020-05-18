4e33d8f9d4488c41e60c9b0a3c1f792f8c0adf81
rahulkusuma1999 <55886415+rahulkusuma1999@users.noreply.github.com>
2020-05-18 19:29:53 +0530

    Add files via upload

---
+++ cut the sticks.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+if __name__ == '__main__':
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    while arr:
+        print(len(arr))
+        arr = [x for x in arr if x != min(arr)]
