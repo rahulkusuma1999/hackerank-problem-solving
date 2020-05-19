a0b6e8c03a79f26e74a460c4c4789c6c42b7d004
rahulkusuma1999 <55886415+rahulkusuma1999@users.noreply.github.com>
2020-05-19 21:19:55 +0530

    Add files via upload

---
+++ angry professor.py
@@ -0,0 +1,40 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the angryProfessor function below.
+def angryProfessor(k, a):
+    late=[]
+    ontime=[]
+    for i in range(len(a)):
+        if a[i]<=0:
+            late.append(i)
+        #else:
+        #    ontime.append(i)
+    #if len(late) > k:
+      #  return print('YES')
+    #else:
+     #   return 'NO'
+    return 'YES' if (len(late)<k) else 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
